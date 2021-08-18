79ba04babaef570d015142d9c925724d73a2d729
Glenn Jackson <glennjackson2121@gmail.com>
2021-08-18 15:37:19 -0400

    Correcting Redux DevTools In Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/rootReducer";
-import devToolsEnhancer from 'remote-redux-devtools';
+
 
 export default createStore(
   rootReducer,
-  devToolsEnhancer()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
